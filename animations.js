d6fd8430250267c8df1846dba594da20848affdf
nerds-are-in <annacantthinkofagoodaddress@hotmail.co.uk>
2022-04-07 01:48:00 +0100

    trying to get load ani to only fire once...

--- animations.js
+++ animations.js
@@ -1,9 +1,29 @@
-const nav = document.querySelector("nav");
 import headers from "./dropdown.js";
+const nav = document.querySelector("nav");
+const links = document.querySelectorAll(".header a");
+
+const path = window.location.pathname;
+const page = path.split("/").pop();
+console.log(path.split("/"));
+console.log({path});
+console.log({page});
+
 import {mobileWidth} from "./mobile.js";
 
-window.onload = () => {      
-    nav.classList.add("visible");
-    headers.forEach(header => header.classList.add("visible"));
-    console.log("classes added");    
-};
+window.addEventListener("load", loadPage, {once: true});
+
+function loadPage() {
+    if (document.title === "Home") {
+        nav.style.opacity = "1";
+        nav.style.height = "90px";
+        headers.forEach(header => {
+            header.style.height = "90px";
+            header.style.opacity = "1";
+        });
+        links.forEach(link => {
+            link.style.opacity = "1";
+        });
+        console.log("classes added");  
+       // window.removeEventListener("load", loadPage);
+    }   
+}
--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-/*const nav = document.querySelector("nav");
-import headers from "./dropdown.js";
-import {mobileWidth} from "./mobile.js";
-
-window.onload = () => {
-    nav.classList.add("visible");
-    headers.forEach(header => header.classList.add("visible"));
-    console.log("classes added");
-};*/
-
 import "./animations.js";
 import "./dropdown.js";
 import "./mobile.js";
 import "./portfolio.js";
+
+
 
 
