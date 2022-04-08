e63ef07c0c881f4643066cffecfd9d49ff336a3f
nerds-are-in <annacantthinkofagoodaddress@hotmail.co.uk>
2022-04-08 02:12:49 +0100

    having problems with conflicting animations

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-import "./animations.js";
+//import "./animations.js";
 import "./dropdown.js";
 import "./mobile.js";
 import "./portfolio.js";
 
+const controlsContainer = document.querySelector("section#controls-container");
+
+setTimeout(() => {
+    controlsContainer.style.animation = "none";
+    controlsContainer.style.opacity = "1";
+    controlsContainer.style.transform = "scale(1)";
+}, 5000);
 
 
 
